954bf43754f9eea0e0bb16b6cd22706a79e97f85
Randall Spencer <randallspencer@gmail.com>
2016-03-19 02:17:24 -0700

    Fix configs for Heroku pgsql.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -5,3 +7,20 @@ app = Flask(__name__)
 app.config.from_object('config')
 from app import views, models
 db = SQLAlchemy(app)
+
+if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-if os.environ.get('HEROKU_POSTGRESQL_BLACK_URL') is None:
+if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = os.environ['LOCAL_DATABASE_URI']
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_BLACK_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
