d62c6ccbc4b6169e3cc72214c45938a861ca8cd5
Randall Spencer <randallspencer@gmail.com>
2016-03-19 12:21:35 -0700

    Add LoginManager to app/__init__.py.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ import os
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = "public for now"
 app.config['SESSION_TYPE'] = 'filesystem'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 import views, models
 db = SQLAlchemy(app)
 
