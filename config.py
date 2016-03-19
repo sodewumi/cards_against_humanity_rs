194c0259062ee6aad42d10e6e2b377273538e0bb
Randall Spencer <randallspencer@gmail.com>
2016-03-19 10:52:23 -0700

    Disable WTF_CSRF_ENABLED in config.py.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-WTF_CSRF_ENABLED = True
+WTF_CSRF_ENABLED = False
 
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = os.environ['LOCAL_DATABASE_URI']
