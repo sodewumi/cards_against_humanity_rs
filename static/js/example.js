594b989d49041c8b72f68174205443d289880963
Stella Odewumi <stellaodewumi@gmail.com>
2016-04-11 20:39:47 -0500

    Added mobile responsive game room for czar.

--- static/js/example.js
+++ static/js/example.js
@@ -1,4 +1,7 @@
 var _ = require('underscore')
 var $ =require('jquery')
-document.write(_.shuffle([1,2,3,4]));
+
+require("../css/style.css")
+
+console.log(_.shuffle([1,2,3,4]));
 document.write(require("./example2.js"));
